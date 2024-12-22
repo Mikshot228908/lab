9f95b3e3329b0e6b1340e735f751edc79743df06
Shurenkov Nikita <nik.shurenkov@mail.ru>
2024-12-22 23:07:06 +0300

    "Commit"

--- authController.js
+++ authController.js
@@ -2,6 +2,17 @@ const User = require("./model/User");
 const Role = require("./model/Role");
 const bcrypt = require("bcryptjs");
 const { validationResult } = require("express-validator");
+const jwt = require('jsonwebtoken')
+const {secret} = require('./config')
+
+const generateAccessToken = (id,roles) =>{
+    const payload ={
+      id,
+      roles
+    }
+    return jwt.sign(payload,secret,{expiresIn:"24h"})
+}
+
 
 class authController {
   async registration(req, res) {
@@ -21,7 +32,7 @@ class authController {
       }
 
       const hashPassword = bcrypt.hashSync(password, 7);
-      const userRole = await Role.findOne({ value: "USER" });
+      const userRole = await Role.findOne({ value: "ADMIN" });
       const user = new User({
         username,
         password: hashPassword,
@@ -38,6 +49,17 @@ class authController {
 
   async login(req, res) {
     try {
+        const {username,password} = req.body
+        const user = await User.findOne({username})
+        if(!user){
+            return res.status(400).json({message:`Пользователь${username} не найден`})
+        }
+        const validPassword = bcrypt.compareSync(password,user.password)
+        if(!validPassword){
+            return res.status(400).json({message:`Введен неверный пароль`})
+        }
+        const token = generateAccessToken(user._id,user.roles)
+        return res.json({token})
     } catch (e) {
       console.log(e);
       res.status(400).json({ message: "Login Error!!!!!" });
@@ -46,7 +68,8 @@ class authController {
 
   async getUsers(req, res) {
     try {
-      res.json("server work");
+      const users = await User.find()
+      res.json(users);
     } catch (e) {
       console.log(e);
       res.status(400).json({ message: "Error getting users" });
